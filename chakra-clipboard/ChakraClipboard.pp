4220e3fb75dac46e1adfcfc479d4168af92df6ef
danielrippa <621091+danielrippa@users.noreply.github.com>
2023-03-17 19:20:30 -0300

    .

--- chakra-clipboard/ChakraClipboard.pp
+++ chakra-clipboard/ChakraClipboard.pp
@@ -1,1 +1,35 @@
+unit ChakraClipboard;
 
+{$mode delphi}
+
+interface
+
+  uses ChakraTypes;
+
+  function GetJsValue: TJsValue;
+
+implementation
+
+  uses Chakra, ChakraUtils, Win32Clipboard;
+
+  function ClipboardGetText(Args: PJsValue; ArgCount: Word): TJsValue;
+  begin
+    Result := StringAsJsString(GetClipboardText);
+  end;
+
+  function ClipboardSetText(Args: PJsValue; ArgCount: Word): TJsValue;
+  begin
+    CheckParams('setText', Args, ArgCount, [jsString], 1);
+    Result := BooleanAsJsBoolean(SetClipboardText(JsStringAsString(Args^)));
+  end;
+
+  function GetJsValue;
+  begin
+    Result := CreateObject;
+
+    SetFunction(Result, 'getText', ClipboardGetText);
+    SetFunction(Result, 'setText', ClipboardSetText);
+
+  end;
+
+end.
